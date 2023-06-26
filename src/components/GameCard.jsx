6ebc1cc811d7017cf23f4ddfa732965dab411c57
Javier Ospina <jaheosig10@gmail.com>
2023-06-26 17:37:37 -0500

    colores botones y tarjeta faltan fondo

---
+++ src/components/GameCard.jsx
@@ -0,0 +1,19 @@
+const GameCard = ({data}) => {
+  return (
+    <div>
+      <article className="card__countainer">
+        <h1>Galleta de la Fortuna</h1>
+        <div className="phrase__container">
+          <p className="phrase">{data.phrase}</p>
+        </div>
+      </article>
+      <div className="author__countainer">
+        <article className="card__author">
+          <p>{data.author}</p>
+        </article>
+      </div>
+    </div>
+  );
+};
+
+export default GameCard;
