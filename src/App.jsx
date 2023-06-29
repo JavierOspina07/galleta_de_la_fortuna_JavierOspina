3aae70547bc7dfe67fba8994c809ae4918436b72
Javier Ospina <jaheosig10@gmail.com>
2023-06-28 23:44:14 -0500

    escritura

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,42 @@
 import "./App.css";
-import phrases from "./data/phrases.json"
-import GameCard from './/components/GameCard'
+import phrases from "./data/phrases.json";
+import GameCard from "./components/GameCard";
 import { useState } from "react";
 
 function App() {
-
-  const images = [ 
-    '../img/fondo1.png',
-    '../img/fondo1.png',
-    '../img/fondo2.png',
-    '../img/fondo2.png',
-    '../img/fondo3.png',
-    '../img/fondo3.png',
-    '../img/fondo4.png',
-    '../img/fondo5.png',
-    '../img/fondo5.png',
-    '../img/fondo6.png',
-    '../img/fondo6.png',
-    '../img/fondo7.png',
-    '../img/fondo7.png',
-    '../img/fondo8.png',
-    '../img/fondo8.png',
-  ]
-  const [index, setIndex] = useState(0)
+  const images = [
+    "../img/fondo1.png",
+    "../img/fondo1.png",
+    "../img/fondo2.png",
+    "../img/fondo2.png",
+    "../img/fondo3.png",
+    "../img/fondo3.png",
+    "../img/fondo4.png",
+    "../img/fondo5.png",
+    "../img/fondo5.png",
+    "../img/fondo6.png",
+    "../img/fondo6.png",
+    "../img/fondo7.png",
+    "../img/fondo7.png",
+    "../img/fondo8.png",
+    "../img/fondo8.png",
+  ];
+  const [index, setIndex] = useState(0);
 
   const changePhrase = () => {
     setIndex(Math.floor(Math.random() * phrases.length));
   };
 
+  document.body.style = `background: url(${images[index]}) no-repeat center center fixed; 
+  background-size: cover; text-align: center`;
 
-  document.body.style = `background: url(${images[index]}) no-repeat center center fixed; background-size: cover; text-align: center`;
-
-  
   return (
     <>
-      <GameCard 
-      data={phrases[index]}
-      />
+      <GameCard data={phrases[index]} />
 
-      <button onClick={changePhrase} className="btn__fortune">fortuna</button>
+      <button onClick={changePhrase} className="btn__fortune">
+        fortuna
+      </button>
     </>
   );
 }
